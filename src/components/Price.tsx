4c9f7c7a29183725fcb68a1c432cd156b0c8a8ab
Diego Lameira <diegolameira@me.com>
2018-06-24 13:41:21 -0300

    feat(components): add price component

---
+++ src/components/Price.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+import { styl } from '@configs/theme';
+
+interface Props {
+  value: number;
+}
+
+export default ({ value }: Props) => {
+  const price = parser(value);
+  return (
+    <Price>
+      {price[0]}
+      <Cents>{`,${price[1] || '00'}`}</Cents>
+    </Price>
+  );
+};
+
+const parser = (price: number) => (price + '').split('.');
+
+const Price = styl(styled.Text)`
+  font-size: 30px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.foreground};
+`;
+
+const Cents = styl(styled.Text)`
+  font-size: 20px;
+`;
