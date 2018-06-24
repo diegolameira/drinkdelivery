adb6e6b5ea4e619d807ae131dbc55d2fd9ad5a4f
Diego Lameira <diegolameira@me.com>
2018-06-24 14:43:45 -0300

    style(App): containers box-modeling adjustments

--- App.tsx
+++ App.tsx
@@ -2,22 +2,31 @@ import React from 'react';
 import { View } from 'react-native';
 import { NativeRouter, Route } from 'react-router-native';
 import { ThemeProvider } from 'styled-components';
+import styled from 'styled-components/native';
 
-import { Theme } from '@configs/theme';
+import { Theme, styl } from '@configs/theme';
 import Header from '@components/Header';
 import Home from '@pages/Home';
 import Products from '@pages/Products';
 
 export default () => (
   <ThemeProvider theme={new Theme()}>
-    <View>
+    <Container>
       <Header />
       <NativeRouter>
-        <View>
+        <Main>
           <Route exact path="/" component={Home} />
           <Route path="/products" component={Products} />
-        </View>
+        </Main>
       </NativeRouter>
-    </View>
+    </Container>
   </ThemeProvider>
 );
+
+const Container = styl(styled.View)`
+  flex: 1;
+`;
+
+const Main = styl(styled.View)`
+  flex: 1;
+`;
